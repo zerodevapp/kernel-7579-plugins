247acb9bb7b0d0866fdbab3da288049db7f5a69e
adnpark <aidenp.dev@gmail.com>
2024-04-02 17:55:21 +0900

    feat: add userIds counter for sudo policy

--- policies/sudo/src/SudoPolicy.sol
+++ policies/sudo/src/SudoPolicy.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.0;
 import "kernel/sdk/moduleBase/PolicyBase.sol";
 
 contract SudoPolicy is PolicyBase {
-    // TODO: how should we handle this?
+    mapping(address => uint256) public usedIds;
+
     function isInitialized(address wallet) external view override returns (bool) {
-        return false;
+        return usedIds[wallet] > 0;
     }
 
     function checkUserOpPolicy(bytes32 id, PackedUserOperation calldata userOp)
@@ -27,7 +28,11 @@ contract SudoPolicy is PolicyBase {
         return 0;
     }
 
-    function _policyOninstall(bytes32 id, bytes calldata _data) internal override {}
+    function _policyOninstall(bytes32 id, bytes calldata _data) internal override {
+        usedIds[msg.sender]++;
+    }
 
-    function _policyOnUninstall(bytes32 id, bytes calldata _data) internal override {}
+    function _policyOnUninstall(bytes32 id, bytes calldata _data) internal override {
+        usedIds[msg.sender]--;
+    }
 }
