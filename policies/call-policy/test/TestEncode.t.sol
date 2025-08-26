fb9c55ad0d82c59c9ac52a2ac801c29592e5974d
leekt <leekt216@gmail.com>
2025-08-26 23:41:35 +0900

    oneof

--- policies/call-policy/test/TestEncode.t.sol
+++ policies/call-policy/test/TestEncode.t.sol
@@ -10,8 +10,8 @@ contract MockCallee {
 
     bytes public baz;
 
-    function foo(uint256 _bar, bytes memory _baz) external payable {
-        bar = _bar;
+    function foo(uint256 _bar1, uint256 _bar2, bytes memory _baz) external payable {
+        bar = _bar1 + _bar2;
         baz = _baz;
     }
 }
@@ -49,6 +49,8 @@ contract TestEncoding is Test {
             return bytes32(uint256(param + random));
         } else if (condition == ParamCondition.NOT_EQUAL) {
             return bytes32(uint256(param));
+        } else if (condition == ParamCondition.ONE_OF) {
+            return keccak256(abi.encodePacked(param, 10 + random));
         }
         return bytes32(0);
     }
@@ -69,6 +71,8 @@ contract TestEncoding is Test {
             return bytes32(uint256(param) - random + 1);
         } else if (condition == ParamCondition.NOT_EQUAL) {
             return bytes32(uint256(param) + random);
+        } else if (condition == ParamCondition.ONE_OF) {
+            return keccak256(abi.encodePacked(param, random % 10));
         }
         return bytes32(0);
     }
@@ -137,6 +141,104 @@ contract TestEncoding is Test {
         uint256 random
     ) external {
         testSingle(5, valueLimit, value, anyTarget, res, param, random);
+    }
+
+    function testMultipleParams(
+        uint8 c1,
+        uint8 c2,
+        uint256 valueLimit,
+        uint256 value,
+        bool anyTarget,
+        uint8 res,
+        uint256 param1,
+        uint256 random1,
+        uint256 param2,
+        uint256 random2
+    ) external {
+        vm.assume(c1 <= uint8(ParamCondition.NOT_EQUAL));
+        vm.assume(c2 <= uint8(ParamCondition.NOT_EQUAL));
+        vm.assume(valueLimit < type(uint256).max);
+        vm.assume(value <= valueLimit);
+        vm.assume(res <= uint8(Result.TargetViolation));
+
+        ParamCondition condition1 = ParamCondition(c1);
+        ParamCondition condition2 = ParamCondition(c2);
+        param1 = param1 % (uint256(type(uint128).max) + 1) + 1;
+        random1 = (random1 % param1) + 1;
+        param2 = param2 % (uint256(type(uint128).max) + 1) + 1;
+        random2 = (random2 % param2) + 1;
+
+        if (anyTarget && res == 3) {
+            res--;
+        }
+        Result result = Result(res);
+
+        bytes32 id = bytes32(bytes4(0x12345678));
+        address owner = makeAddr("Owner");
+        bytes4 selector = mock.foo.selector;
+
+        bytes32[] memory params1 = new bytes32[](1);
+        params1[0] = bytes32(param1);
+        bytes32[] memory params2 = new bytes32[](1);
+        params2[0] = bytes32(param2);
+        ParamRule[] memory rules = new ParamRule[](2);
+        rules[0] = ParamRule({condition: condition1, offset: 0x00, params: params1});
+        rules[1] = ParamRule({condition: condition2, offset: 0x20, params: params2});
+
+        Permission memory p = Permission({
+            callType: CallType.wrap(0x00),
+            target: anyTarget ? address(0) : address(mock),
+            selector: selector,
+            valueLimit: valueLimit,
+            rules: rules
+        });
+
+        Permission[] memory perms = new Permission[](1);
+        perms[0] = p;
+
+        vm.startPrank(owner);
+        callPolicy.onInstall(abi.encodePacked(id, abi.encode(perms)));
+        vm.stopPrank();
+
+        // check if pass
+        PackedUserOperation memory op = PackedUserOperation({
+            sender: owner,
+            nonce: 0,
+            initCode: hex"",
+            callData: abi.encodeWithSelector(
+                IERC7579Account.execute.selector,
+                bytes32(0),
+                ExecLib.encodeSingle(
+                    result == Result.TargetViolation ? address(1) : address(mock),
+                    result == Result.ValueViolation ? valueLimit + 1 : value,
+                    abi.encodeWithSelector(
+                        selector,
+                        result == Result.ParamRuleViolation && (random1 >= random2)
+                            ? wrongParam(condition1, param1, random1)
+                            : goodParam(condition1, param1, random1),
+                        result == Result.ParamRuleViolation && (random1 <= random2)
+                            ? wrongParam(condition2, param2, random2)
+                            : goodParam(condition2, param2, random2),
+                        hex"deadbeef"
+                    )
+                )
+            ),
+            accountGasLimits: bytes32(0),
+            preVerificationGas: 0,
+            gasFees: bytes32(0),
+            paymasterAndData: hex"",
+            signature: hex""
+        });
+        vm.startPrank(owner);
+        if (result == Result.ParamRuleViolation) {
+            vm.expectRevert(CallPolicy.CallViolatesParamRule.selector);
+        } else if (result == Result.ValueViolation) {
+            vm.expectRevert(CallPolicy.CallViolatesValueRule.selector);
+        } else if (result == Result.TargetViolation) {
+            vm.expectRevert(CallPolicy.CallViolatesTargetRule.selector);
+        }
+        callPolicy.checkUserOpPolicy(id, op);
+        vm.stopPrank();
     }
 
     function testSingle(
@@ -221,6 +323,95 @@ contract TestEncoding is Test {
             vm.expectRevert(CallPolicy.CallViolatesTargetRule.selector);
         }
         callPolicy.checkUserOpPolicy(id, op);
+        vm.stopPrank();
+    }
+
+    function testOneOf(
+        uint256 valueLimit,
+        uint256 value,
+        bool anyTarget,
+        uint8 res,
+        uint256 param,
+        uint256 random
+    ) external {
+        vm.assume(valueLimit < type(uint256).max);
+        vm.assume(value <= valueLimit);
+        vm.assume(res <= uint8(Result.TargetViolation));
+
+        param = param % (uint256(type(uint128).max) + 1) +1;
+        random = (random % param) + 1;
+        
+        if(anyTarget && res == 3) {
+            res--;
+        }
+        Result result = Result(res);
+
+        bytes32 id = bytes32(bytes4(0x12345678));
+        address owner = makeAddr("Owner");
+        bytes4 selector = mock.foo.selector;
+
+        bytes32[] memory params = new bytes32[](10);
+        for(uint256 i = 0; i < 10; i++) {
+            params[i] = keccak256(abi.encodePacked(param, i));
+        }
+        ParamRule[] memory rules = new ParamRule[](1);
+        rules[0] = ParamRule({
+            condition : ParamCondition.ONE_OF,
+            offset : 0x00,
+            params : params
+        });
+
+        Permission memory p = Permission({
+            callType : CallType.wrap(0x00),
+            target : anyTarget ? address(0) : address(mock),
+            selector : selector,
+            valueLimit : valueLimit,
+            rules : rules
+        });
+
+        Permission[] memory perms = new Permission[](1);
+        perms[0] = p;
+
+        vm.startPrank(owner);
+        callPolicy.onInstall(abi.encodePacked(id, abi.encode(perms)));
+        vm.stopPrank();
+
+        // check if pass
+        PackedUserOperation memory op = PackedUserOperation({
+            sender : owner,
+            nonce : 0,
+            initCode : hex"",
+            callData : abi.encodeWithSelector(
+                IERC7579Account.execute.selector,
+                bytes32(0),
+                ExecLib.encodeSingle(
+                    result == Result.TargetViolation? address(1) : address(mock),
+                    result == Result.ValueViolation ? valueLimit + 1 : value,
+                    abi.encodeWithSelector(
+                        selector,
+                        result == Result.ParamRuleViolation ? wrongParam(ParamCondition.ONE_OF, param, random) : goodParam(ParamCondition.ONE_OF, param, random),
+                        hex"deadbeef"
+                    )
+                )
+            ),
+            accountGasLimits : bytes32(0),
+            preVerificationGas : 0,
+            gasFees : bytes32(0),
+            paymasterAndData : hex"",
+            signature : hex""
+        });
+        vm.startPrank(owner);
+        if(result == Result.ParamRuleViolation) { 
+            vm.expectRevert(CallPolicy.CallViolatesParamRule.selector);
+        } else if(result == Result.ValueViolation) {
+            vm.expectRevert(CallPolicy.CallViolatesValueRule.selector);
+        } else if(result == Result.TargetViolation) {
+            vm.expectRevert(CallPolicy.CallViolatesTargetRule.selector);
+        }
+        callPolicy.checkUserOpPolicy(
+            id,
+            op
+        );
         vm.stopPrank();
     }
 }
