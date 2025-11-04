8acbec3e647044538f636e5d14321978da321a25
leekt <leekt216@gmail.com>
2025-11-04 23:06:14 +0900

    merge recent commits

--- policies/call-policy/lib/kernel/src/types/Structs.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
-
-struct Execution {
-    address target;
-    uint256 value;
-    bytes callData;
-}
--- policies/call-policy/lib/kernel/test/Kernel.t.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.0;
-
-import {KernelTestBase} from "../src/sdk/KernelTestBase.sol";
-
-contract KernelTest is KernelTestBase {}
